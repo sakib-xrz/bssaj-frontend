27f9e15ea1134995627fb72028367363d959ed44
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-06-26 01:40:55 +0600

    section header common component done

---
+++ app/(public)/about/page.tsx
@@ -0,0 +1,11 @@
+import About from "./about";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <About />
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ components/shared/section-header.tsx
@@ -0,0 +1,26 @@
+import { cn } from "@/lib/utils";
+
+interface SectionHeaderProps {
+  title: string;
+  description?: string;
+  className?: string;
+}
+
+export default function SectionHeader({
+  title,
+  description,
+  className,
+}: SectionHeaderProps) {
+  return (
+    <div className={cn("text-center space-y-2", className)}>
+      <h2 className="text-4xl font-extrabold tracking-tight sm:text-5xl md:text-5xl leading-tight text-primary mb-6">
+        {title}
+      </h2>
+      {description && (
+        <p className="text-muted-foreground max-w-2xl mx-auto text-wrap">
+          {description}
+        </p>
+      )}
+    </div>
+  );
+}
