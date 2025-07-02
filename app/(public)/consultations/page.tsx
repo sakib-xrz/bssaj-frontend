638d12fd0ca5038eaccfabede53b5c28c5fa1be6
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-07-02 13:04:00 +0600

    consultatioin page done

---
+++ app/(public)/consultations/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Consultations from "./consultation";
+
+const ConsultationsPage = () => {
+  return (
+    <div>
+      <Consultations />
+    </div>
+  );
+};
+
+export default ConsultationsPage;
